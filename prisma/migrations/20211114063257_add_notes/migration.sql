08fcb41200d30ad0d5433ece16787d49e25be2f3
silvesterwali <silvesterlhwali123@gmail.com>
2021-11-14 14:35:34 +0800

    adding notes feature

---
+++ prisma/migrations/20211114063257_add_notes/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `Notes` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(191) NOT NULL,
+    `craetedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
